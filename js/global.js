81e32501a03155ebec81c625ee1b5f746db185b6
root <root@ip-10-151-109-226.ec2.internal>
2013-04-18 20:19:20 +0000

    New version of homepage.  All of Jennifer's initial changes.  Heatmap function added

---
+++ js/global.js
@@ -0,0 +1,96 @@
+/* Heatmap stats */
+window.onload = function(){
+ 
+    // heatmap configuration
+    var config = {
+        element: document.getElementById("full"),
+        radius: 30,
+        visible: false,
+        opacity: 50
+    };
+    
+    //creates and initializes the heatmap
+    window.heatmap = h337.create(config);
+ 
+    var active = true,
+        idle = false,
+        over = false,
+        x = 0,
+        y = 0,
+        simulate = false;
+ 
+    // activate capture mode
+    setInterval(function(){
+        active = true;
+    }, 80);
+ 
+    // check whether the mouse is idling
+    var idlechecker = setInterval(function(){
+        if(over && !simulate){
+            // if it's idling -> start the simulation 
+            // and add the last x/y coords
+            simulate = setInterval(function(){
+                window.heatmap.store.addDataPoint(x, y, 1);
+            }, 1000);
+        }
+    }, 150);
+        
+    var add = function(e){
+        x = e.layerX;
+        y = e.layerY
+        window.heatmap.store.addDataPoint(x, y, 1);
+    };
+ 
+    var element = document.getElementById("full");
+ 
+    element.onmousemove = function(e){
+        over = true;
+        if(simulate){
+            clearInterval(simulate);
+            simulate = false;
+        }
+ 
+        if(active){
+            add(e);
+            active = false;
+        }
+    };
+    element.onclick = function(e){
+        add(e);
+    };
+    element.onmouseout = function(){
+        over = false;
+    };
+};
+
+/*
+var config = {
+    "radius": 30,
+    "element": "full",
+    "visible": true,
+    "opacity": 40,
+    "gradient": { 0.45: "rgb(0,0,255)", 0.55: "rgb(0,255,255)", 0.65: "rgb(0,255,0)", 0.95: "yellow", 1.0: "rgb(255,0,0)" }
+};
+
+var heatmap = heatmapFactory.create(config);
+*/
+
+var save_heatmap = function callback(dataSet) {
+   dataSet.screen_width = $(window).width(); 
+   dataSet.screen_height = $(window).height();
+   dataSet.browser = $.browser;
+   $.ajax({
+      type: 'POST',
+      async: false,
+      url: 'controllers/heatmap_controller.php',
+      dataType: 'json',
+      data: 'session=' + JSON.stringify(dataSet),
+      success: function (response) {
+      }
+   });
+};
+
+window.onbeforeunload = function (e) {
+   var dataSet = window.heatmap.store.exportDataSet();
+   save_heatmap(dataSet);
+};
